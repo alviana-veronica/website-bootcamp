d772ffaeb44dc8ba8dc03b5b6bd2a870b41c43a3
“alviana-veronica-coencept” <veronicalviana.me@gmail.com>
2024-10-13 20:28:52 +0700

    initial layout

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+// const { ROUTE } = require("@snipkode/server");
+
 require("@snipkode/server");
 //console.log("ini global object", global);
+
+ROUTE("GET /", function () {
+  this.view("pages/landingpage");
+});
+
+ROUTE("GET /todo", function () {
+  this.view("pages/todo");
+});
+
+ROUTE("GET /dashboard", function () {
+  this.view("pages/dashboard");
+});
 
 HTTP("debug", { port: 80 });
